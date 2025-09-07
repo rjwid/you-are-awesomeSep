34501f32e80ecdbe8d7b52f23950f9d9ade219e7
Rj Wid <rwidrow@yahoo.com>
2025-09-06 20:51:26 -0700

    Initial Commit

---
+++ you are awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  you are awesome
+//
+//  Created by RJ Widrow on 9/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ you are awesomeTests/you_are_awesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  you_are_awesomeTests.swift
+//  you are awesomeTests
+//
+//  Created by RJ Widrow on 9/6/25.
+//
+
+import Testing
+@testable import you_are_awesome
+
+struct you_are_awesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
